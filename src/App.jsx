154ae1faf679d21b865eea0f9b49952c9ba955cb
Sachin kathar <136295822+SACHINKATHAR2005@users.noreply.github.com>
2025-06-30 15:55:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-
-
 import React from 'react'
+import { Link, Route, Routes, Navigate } from 'react-router-dom'
 
-import { Link, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
-import './index.css'  
+import './index.css'
 import MovieDetails from './pages/movieDetails/MOvieDetails'
 import Movies from './pages/Movies/Movies'
 import MyList from './pages/myList/MyList'
@@ -12,22 +10,24 @@ import Latest from './pages/latest/Latest'
 import TvShow from './pages/TvShow/TvShow'
 
 function App() {
- 
-
   return (
     <>
-     
       <Routes>
-    <Route path='/Home' element = {<Home/>}/>
-    <Route path='/movieDetails/:id' element = {<MovieDetails/>}/>
-    <Route path='/movies' element={<Movies/>}/>
-    <Route path='/myList/:id' element={<MyList/>}/>
-    <Route path='*' element={<h1>Not Found</h1>}/>
-    <Route path='/latest' element={<Latest/>}/>
-    <Route path='/tvshow' element={<TvShow/>}/>
+        {/* Default route to redirect to /Home */}
+        <Route path='/' element={<Navigate to="/Home" />} />
+
+        <Route path='/Home' element={<Home />} />
+        <Route path='/movieDetails/:id' element={<MovieDetails />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/myList/:id' element={<MyList />} />
+        <Route path='/latest' element={<Latest />} />
+        <Route path='/tvshow' element={<TvShow />} />
+
+        {/* Catch-all route for 404 */}
+        <Route path='*' element={<h1>Not Found</h1>} />
       </Routes>
 
-      <Link to={'/Home'}>Go to Home page </Link>
+      <Link to={'/Home'}>Go to Home page</Link>
     </>
   )
 }
